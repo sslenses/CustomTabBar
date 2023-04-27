f0b879c92e0fe7c045c4393772ccafa2b5364f83
sslenses <sidiqslenses@gmail.com>
2023-04-27 10:18:37 +0800

    first commit

--- CustomTabBar/ContentView.swift
+++ CustomTabBar/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                Text("Dast ist lange her.")
+                Spacer()
+                HStack {
+                    TabBarIcon(width: geometry.size.width/5, height: geometry.size.height/28, systemIconName: "house", tabName: "Home")
+                    TabBarIcon(width: geometry.size.width/5, height: geometry.size.height/28, systemIconName: "scooter", tabName: "Scooter")
+                    TabBarIcon(width: geometry.size.width/5, height: geometry.size.height/28, systemIconName: "qrcode.viewfinder", tabName: "Scan Me")
+                    TabBarIcon(width: geometry.size.width/5, height: geometry.size.height/28, systemIconName: "parkingsign.circle", tabName: "EndHub")
+                    TabBarIcon(width: geometry.size.width/5, height: geometry.size.height/28, systemIconName: "creditcard", tabName: "Wallet")
+                }
+                .frame(width: geometry.size.width, height: geometry.size.height/8)
+                .background(Color("TabBarBackground"))
+                .shadow(radius: 2)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct TabBarIcon: View {
+    
+    let width, height: CGFloat
+    let systemIconName, tabName: String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: systemIconName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: width, height: height)
+                .padding(.top, 10)
+            Text (tabName)
+                .font(.footnote)
+            Spacer ()
+        }
     }
 }
